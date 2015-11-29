4fc8872a340dbfd9dd5cc0de7ad61bc95ac66f2a
Yorrick Jansen <yorrick.jansen@gmail.com>
2015-11-29 17:49:54 -0500

    Journal referential

--- extract/journals.py
+++ extract/journals.py
@@ -6,10 +6,33 @@ import json
 
 class JournalReferential():
 
-    def __init__(self):
-        pass
+    def __init__(self, journals):
+        self.journals = journals
+        self.journal_names = self.build_reverse_dict(self.journals, _get_journal_names, _get_journal_id)
+
+    @classmethod
+    def build_reverse_dict(cls, iterable, key_extractor, value_extractor = lambda x: x):
+        result = {}
+
+        for item in iterable:
+            key = key_extractor(item)
+            value = value_extractor(item)
+
+            # support for multi valuated keys
+            if isinstance(key, list):
+                for k in key:
+                    result[k] = value
+
+        return result
 
 
 def build_journal_referential(file_path):
     with codecs.open(file_path, "r", "utf-8") as journal_file:
-        return json.load(journal_file)
+        return JournalReferential(json.load(journal_file))
+
+
+def _get_journal_names(journal):
+    return [name["url_name"] for name in journal["names"]]
+
+def _get_journal_id(journal):
+    return journal["id"]
--- tests/TestJournal.py
+++ tests/TestJournal.py
@@ -9,7 +9,15 @@ BASE_DIR = path.dirname(path.abspath(__file__))
 class TestExtract(TestCase):
 
     def test_build_journal_referential(self):
-        journals = build_journal_referential(path.join(BASE_DIR, "journals.json"))
+        referential = build_journal_referential(path.join(BASE_DIR, "test-journals.json"))
+        self.assertEquals(len(referential.journals), 3)
+        self.assertEquals(len(referential.journal_names), 4)
+        self.assertEquals(referential.journal_names, {
+            "ac": "crimino",
+            "crimino": "crimino",
+            "acadiensis": "acadiensis",
+            "ae": "ae",
+        })
 
     def test_build_full_referential(self):
         # tests that no fail happens
