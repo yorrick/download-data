e4fe2c226a2827b546ad421a006cfe81a0a5cd25
Yorrick Jansen <yorrick.jansen@gmail.com>
2016-01-10 16:34:47 -0500

    Robot detection implementation

--- visualization/robots-activity.R
+++ visualization/robots-activity.R
@@ -6,11 +6,12 @@ library("ggplot2")
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "logs", host = "localdocker", port = 5432, user = "postgres", password = "postgres")
 
-query <- paste('select download_hour, count(*) as number, is_robot, is_bad_robot',
+query <- paste('select download_hour, country, count(*) as number, is_robot, is_bad_robot',
                'from download',
                'where', 
                'download_hour is not null',
-               'group by download_hour, is_robot, is_bad_robot', 
+               'and country in (select country from download group by country order by count(*) desc limit 5)',               
+               'group by download_hour, country, is_robot, is_bad_robot', 
                'order by download_hour asc', sep=" ")
 
 hours <- dbGetQuery(con, query) %>% tbl_df %>%
@@ -18,7 +19,8 @@ hours <- dbGetQuery(con, query) %>% tbl_df %>%
     select(-is_robot, -is_bad_robot)
 
 plot <- ggplot(hours, aes(x=download_hour, y=number, colour=activity_type)) +
-    geom_line()
+    geom_line() +
+    facet_grid(. ~ country)
 
 print(plot)
 
