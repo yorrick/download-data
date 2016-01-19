b78881b5013af33b92d609e025b10b0869d51318
Yorrick Jansen <yorrick.jansen@gmail.com>
2016-01-19 16:10:47 -0500

    Load journal data, and use full_os flag to compute embargo flag on downloads

--- sql/tests.sql
+++ sql/tests.sql
@@ -1,9 +1,8 @@
--- test online years on dowloads
-
 do
 $$
 declare
   wrong_online_year integer;
+  wrong_embargo integer;
 begin
   select count(*)
   into wrong_online_year
@@ -12,6 +11,17 @@ begin
 
   if (wrong_online_year > 0) then
     raise exception 'No download should have an online year before publication year';
+  end if;
+
+
+
+  select count(*)
+  into wrong_embargo
+  from download d, journal j
+  where d.journal_id = j.id and d.embargo is true and j.full_oa is true;
+
+  if (wrong_embargo > 0) then
+    raise exception 'No download from a full oa journal should have embargo = true';
   end if;
 end;
 $$
