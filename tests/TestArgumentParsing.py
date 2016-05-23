d8536de19039b617c629a7b49173b2d0c7817449
Yorrick Jansen <yorrick.jansen@gmail.com>
2016-05-23 10:38:30 -0400

    Updated parsing arguments

--- tests/TestArgumentParsing.py
+++ tests/TestArgumentParsing.py
@@ -10,16 +10,16 @@ class TestArgumentParsing(TestCase):
             "script_name.py", "--debug", "--keep-robots",
             "--processes", "7",
             "--print-stats-for-ip", "222.222.222.222",
-            "data/file1.log", "data/file2.log"])
-        self.assertEquals(params.log_files, ["data/file1.log", "data/file2.log"])
+            "/tmp/", "/tmp/"])
+        self.assertEquals(params.source_dir, "/tmp/")
+        self.assertEquals(params.target_dir, "/tmp/")
         self.assertTrue(params.debug)
         self.assertTrue(params.keep_robots)
         self.assertEquals(params.processes, 7)
         self.assertEquals(params.print_stats_for_ip, "222.222.222.222")
 
     def test_parse_argv_default_args(self):
-        params = parse_argv(["script_name.py", "data/file1.log", "data/file2.log"])
-        self.assertEquals(params.log_files, ["data/file1.log", "data/file2.log"])
+        params = parse_argv(["script_name.py", "/tmp/", "/tmp/"])
         self.assertFalse(params.debug)
         self.assertFalse(params.keep_robots)
         self.assertEquals(params.processes, 4)
